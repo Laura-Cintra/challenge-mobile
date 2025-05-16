307f0c517ddf87a3f4ac4560631fdf07aa1d62ee
Laura Cintra <lauraocintra@gmail.com>
2025-05-16 20:53:20 -0300

    fix: removing test page

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Menu from './components/Menu';
+import Login from './pages/LoginForm';
+import UserProvider, { useUser } from './providers/UserContext';
+
+const Stack = createNativeStackNavigator();
+
+function Routes() {
+  const { user } = useUser();
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {user ? (
+        <Stack.Screen name="MainApp" component={Menu} />
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Menu />
-    </NavigationContainer>
+    <UserProvider>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </UserProvider>
   );
 }
