15696d247e27de2068b9733dc81a325c2529ac2a
Laura Cintra <lauraocintra@gmail.com>
2025-05-16 16:22:32 -0300

    feat: motorcycle registration page

--- components/Menu.js
+++ components/Menu.js
@@ -47,7 +47,6 @@ export default function Menu() {
                   style={{
                     width: iconSize,
                     height: iconSize,
-                    resizeMode: 'contain',
                   }}
                 />
               ) : (
