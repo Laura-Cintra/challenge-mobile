cc3ca38be905d782f75ddba5d43db4607716e39b
Laura Cintra <lauraocintra@gmail.com>
2025-11-01 13:04:09 -0300

    feat: theme and language buttons animation

--- src/components/LanguageButton.js
+++ src/components/LanguageButton.js
@@ -1,4 +1,5 @@
 import { View, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { MotiView } from "moti";
 import { useTheme } from "../providers/ThemeContext";
 import { useTranslation } from "react-i18next";
 
@@ -9,31 +10,41 @@ export default function LanguageButton({ topOffset = 88 }) {
   const { colors } = useTheme();
   const { i18n } = useTranslation();
 
-  // Função para mudar o idioma
   const mudarIdioma = (lang) => {
     i18n.changeLanguage(lang);
   };
 
   return (
-    <View style={[styles.langContainer, { top: topOffset }]}>
-      {i18n.language === "pt" && (
-        <TouchableOpacity onPress={() => mudarIdioma("es")}>
-          <Image
-            source={ptFlag}
-            style={[styles.flagIcon, { borderColor: colors.text }]}
-          />
-        </TouchableOpacity>
-      )}
+    <MotiView
+      from={{ opacity: 0, translateY: 20 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{
+        type: "timing",
+        duration: 600,
+        delay: 150,
+      }}
+      style={[styles.langContainer, { top: topOffset }]}
+    >
+      <View>
+        {i18n.language === "pt" && (
+          <TouchableOpacity onPress={() => mudarIdioma("es")}>
+            <Image
+              source={ptFlag}
+              style={[styles.flagIcon, { borderColor: colors.text }]}
+            />
+          </TouchableOpacity>
+        )}
 
-      {i18n.language === "es" && (
-        <TouchableOpacity onPress={() => mudarIdioma("pt")}>
-          <Image
-            source={mexFlag}
-            style={[styles.flagIcon, { borderColor: colors.text }]}
-          />
-        </TouchableOpacity>
-      )}
-    </View>
+        {i18n.language === "es" && (
+          <TouchableOpacity onPress={() => mudarIdioma("pt")}>
+            <Image
+              source={mexFlag}
+              style={[styles.flagIcon, { borderColor: colors.text }]}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </MotiView>
   );
 }
 
@@ -44,7 +55,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     right: 30,
-    top: 88
   },
   flagIcon: {
     width: 32,
--- src/components/ThemeToggleButton.js
+++ src/components/ThemeToggleButton.js
@@ -1,30 +1,41 @@
-import { TouchableOpacity, StyleSheet } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import Feather from "react-native-vector-icons/Feather";
+import { MotiView } from "moti";
 import { useTheme } from "../providers/ThemeContext";
 
 export default function ThemeToggleButton({ topOffset = 105 }) {
   const { toggleTheme, colors, theme } = useTheme();
 
   return (
-    <TouchableOpacity
-      style={[styles.button, { top: topOffset }]}
-      onPress={toggleTheme}
+    <MotiView
+      from={{ opacity: 0, translateY: 20 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{
+        type: "timing",
+        duration: 500,
+        delay: 150,
+      }}
+      style={[styles.container, { top: topOffset }]}
     >
-      <Feather
-        name={theme === "light" ? "moon" : "sun"}
-        size={24}
-        color={colors.primary}
-      />
-    </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={toggleTheme}>
+        <Feather
+          name={theme === "light" ? "moon" : "sun"}
+          size={24}
+          color={colors.primary}
+        />
+      </TouchableOpacity>
+    </MotiView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    right: 15,
+  },
   button: {
     padding: 2,
     alignItems: "center",
     justifyContent: "center",
-    position: "absolute",
-    right: 15,
   },
 });
